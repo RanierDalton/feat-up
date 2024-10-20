2ea42ad86556885b02f577cbcb72b247efefe361
RanierDalton <ranier.couto@sptech.school>
2024-10-20 17:05:40 -0300

    Começar a organizar e escrever a estrutura básica do site

---
+++ site/js/contato.js
@@ -0,0 +1,11 @@
+function enviarMensagem(){
+    // Coletar os inputs 
+
+    // validar os inputs 
+
+    // Juntar os inputs em um dicionário
+
+    // enviar esse dicionário ao backend
+
+    // a resposta do back será mostrada na tela pro usuário
+}
