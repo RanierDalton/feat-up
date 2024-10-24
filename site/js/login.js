756b7f48318b8acb7afcaeb58d3eb88dc0464dbe
RanierDalton <ranier.couto@sptech.school>
2024-10-24 10:24:46 -0300

    Adicionar a tela de modal ao Login

---
+++ site/js/login.js
@@ -0,0 +1,34 @@
+let isVisivel = false;
+let inputApelido = document.getElementById('iptSenha');
+let inputSenha = document.getElementById('iptSenha');
+let modal = document.getElementById('popUp');
+
+function mudarVisibilidade(){
+    if(isVisivel){
+        isVisivel = false;
+        inputSenha.type = 'password';
+        iconSenha.classList.remove('fa-eye-slash');
+        iconSenha.classList.add('fa-eye');
+    } else{
+        isVisivel = true;
+        inputSenha.type = 'text';
+        iconSenha.classList.remove('fa-eye');
+        iconSenha.classList.add('fa-eye-slash'); 
+    }
+}
+
+function entrar(){
+    let apelido = inputApelido.value;
+    let senha = inputSenha.value;
+
+    if(apelido == '' || senha == ''){
+        return modal.showModal();
+    }
+
+    fetch('localhost:3333/', {
+
+    }
+    ).then(message, ()=>{
+
+    });
+}
